fb5d182d6b66e9fdc50be9583267a272a637ae5a
Richard Decal <richard.decal@ncf.edu>
2018-06-21 13:52:22 -0700

    added conky-tree

---
+++ bin/conky-tree/wallpaper05/conky.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A bash script that starts two separate conky configurations
+#
+# this script starts the configs meant for a single-screened system
+#   this means that the trees are placed on each side of the screen opposite of
+#   each other
+
+# starts conky scripts
+conky -dc /home/shit/bin/conky-tree/wallpaper05/conky_left.conf & conky -dc /home/shit/bin/conky-tree/wallpaper05/conky_right.conf & conky -dc /home/shit/bin/conky-tree/wallpaper05/conky_left.conf   -x 1920 & conky -dc /home/shit/bin/conky-tree/wallpaper05/conky_right.conf  -x 1920
