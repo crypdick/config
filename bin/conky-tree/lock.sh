6e3ec4d20c375698bcda4321a55853e61f13ee27
Richard Decal <richard.decal@ncf.edu>
2018-11-21 13:01:58 -0500

    + conky, other stuff

---
+++ bin/conky-tree/lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#https://www.reddit.com/r/i3wm/comments/5w95fp/how_to_get_lockscreen_like_this/
+
+icon="$HOME/bin/conky-tree/gay_space_communism.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 2% -scale 5000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
