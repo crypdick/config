e6c353cc733f852aabfe9f2b701f5e6388f114da
Richard Decal <richard.decal@ncf.edu>
2020-03-22 11:51:43 -0300

    polybar for multimon

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,4 +9,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+#polybar -c ~/.config/polybar/config.ini main &
+
+# multi mon set-up
+# https://github.com/polybar/polybar/issues/763#issuecomment-392960721
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main &
+done
